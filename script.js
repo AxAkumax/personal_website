8e5205ba0faa11eb4e047db42604b06816526210
AxAkumax <akshita.akumalla@gmail.com>
2025-01-17 18:43:21 -0800

    loading screen added

---
+++ script.js
@@ -0,0 +1,11 @@
+// Wait for page to load
+window.addEventListener("load", () => {
+    const loadingScreen = document.getElementById("loading-screen");
+    const mainContent = document.getElementById("main-content");
+  
+    // Add a slight delay for better UX
+    setTimeout(() => {
+      loadingScreen.style.display = "none"; // Hide the loading screen
+      mainContent.style.display = "block"; // Show the main content
+    }, 1000); // 1 second delay
+  });
